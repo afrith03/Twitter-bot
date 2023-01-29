7d0e375d5524a87bad32305ce9df85ca869dac66
afrith03 <afrithshariff20@gmail.com>
2023-01-29 19:11:52 +0530

    Added mode prompts

--- openAi.js
+++ openAi.js
@@ -4,20 +4,24 @@ const configuration = new Configuration({
   apiKey: process.env.OPEN_AI,
 });
 const openai = new OpenAIApi(configuration);
-// let promptArray = [
-//   "write a life quote in english",
-//   "write an inspirational quote in english",
-// ];
-// const getPrompt = () => {
-//   let randPrompt = Math.random() * promptArray.length;
-//   return promptArray[randPrompt];
-// };
+let promptArray = [
+  "write a life quote in english",
+  "write an inspirational quote in english",
+  "Write a funny quote",
+  "Write a qoute on Black life",
+  "Write a qoute Technology"
+
+];
+const getPrompt = (arr) => {
+  let randPrompt = Math.round(Math.random() * (arr.length-1));
+  return arr[randPrompt];
+};
 
 const openAIbot = async () => {
-//   let prompt = getPrompt();
+  let prompt = getPrompt(promptArray);
   const completion = await openai.createCompletion({
     model: "text-davinci-001",
-    prompt:  "write a life quote in english",
+    prompt: prompt,
     max_tokens: 100,
   });
   return completion.data.choices[0].text;
