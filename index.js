632d5c88e79e6448174d6fa5fb595f1d29c979be
afrith03 <afrithshariff20@gmail.com>
2023-03-27 06:02:35 +0530

    random quotes added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const { twitterClient } = require("./twitterClient.js");
 const { openAIbot } = require("./openAi.js");
+
+//random quotes
+const Quote = require("inspirational-quotes");
 //cron Job
 const CronJob = require("cron").CronJob;
 //express
@@ -14,26 +17,34 @@ app.listen(port, () => {
 
 const tweet = async () => {
   try {
-    var tweetText = await openAIbot();
+    var quote = Quote.getQuote();
+    // var tweetText = await openAIbot();
     await twitterClient.v2.tweet(
-      tweetText.toString() + "\n \n Follow for daily quotes #afrithtweets"
+      // tweetText.toString() + "\n \n Follow for daily quotes #afrithtweets"
+
+      //random quotes
+      quote.text.toString() +
+        "\n -" +
+        quote.author.toString() +
+        "\n \n Follow for daily quotes #NodeTweets #openAiTweets"
     );
-    console.log("Tewwt success" + tweetText);
-    return tweetText.toString();
+    // console.log("Tewwt success" + tweetText);
+    console.log("Tewwt success" + quote);
+    return quote.text.toString();
   } catch (e) {
     console.log(e);
   }
 };
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   var t = await tweet();
   res.json({ message: "hello twitter bot", tweet: t });
 });
 
-app.post("/twitter", async(req, res) => {
-    var t = await tweet();
-    res.json({ message: "hello twitter bot", tweet: t });
-  });
+app.post("/twitter", async (req, res) => {
+  var t = await tweet();
+  res.json({ message: "hello twitter bot", tweet: t });
+});
 
 // const cronTweet = new CronJob("30 * * * * *", async () => {
 //   tweet();
